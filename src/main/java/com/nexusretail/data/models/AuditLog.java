d7b96368935072461312764153a2eca3d8e75542
Min Zayar Maung <minzayarmaung2002@gmail.com>
2026-05-27 13:37:28 +0630

    Updated

---
+++ src/main/java/com/nexusretail/data/models/AuditLog.java
@@ -0,0 +1,51 @@
+package com.nexusretail.data.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.sql.Timestamp;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@Table(name = "audit", indexes = {
+        @Index(name = "index_action_name", columnList = "action_name"),
+        @Index(name = "index_maker_id", columnList = "maker_id"),
+        @Index(name = "index_made_on_date", columnList = "made_on_date")
+})
+public class AuditLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String action_name;
+    private String entity_name;
+    private Long entity_id;
+    private Long maker_id;
+    private String maker_name;
+
+    private Timestamp made_on_date;
+
+    private String processing_result;
+
+    @Column(columnDefinition = "TEXT")
+    private String command_as_json;
+
+    private String api_url;
+    private String ip_address;
+    private String error_message;
+
+    private String browser_name;
+    private String operating_system;
+    private String operating_system_name;
+    private String operating_system_version;
+    private String device_model;
+}
--- src/main/java/com/nexusretail/system/audit/controller/AuditController.java
+++ src/main/java/com/nexusretail/system/audit/controller/AuditController.java
@@ -1,8 +1,11 @@
 package com.nexusretail.system.audit.controller;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @Tag(name = "Audit Management", description = "Audit management APIs")
 public class AuditController {
+
+    @GetMapping
+    @Operation(summary = "Get Audit Logs", description = "Retrieve audit logs for system activities")
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "Audit logs retrieved successfully"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    public String getAuditLog(){
+        return "";
+    }
 }
